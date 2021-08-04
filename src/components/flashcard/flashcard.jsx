94388c5c6d7ea393f305872bfc3c0eabb554c0c5
jschenkelberg <jmschenkelberg@gmail.com>
2021-08-04 12:39:24 -0500

    added border and white backgroundto table, increased font size of flashcard text

--- src/components/flashcard/flashcard.jsx
+++ src/components/flashcard/flashcard.jsx
@@ -88,44 +88,7 @@ const Flashcard = ({ cardsById, getCards, deleteFlashcard }) => {
         <div className="col-md-4" />
       </div>
       <hr />
-      {/* <div className="row">
-        <div className="col-md-3" />
-        <div className="col-md-6">
-          <h3>Collection Details {cardsById.collection}</h3>
-          <table className="table table-striped">
-            <thead className="thead-dark">
-              <tr>
-                <th scope="col">Collection ID</th>
-                <th scope="col">Card ID</th>
-                <th scope="col">Question</th>
-                <th scope="col">Answer</th>
-                <th scope="col">Delete</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {cardsById.map(({ id, question, answer, collection }) => (
-                <tr key={id}>
-                  <td>{collection}</td>
-                  <td>{id}</td>
-                  <td>{question}</td>
-                  <td>{answer}</td>
-                  <td>
-                  <button
-                    type="button"
-                    className="btn btn-outline-primary"
-                    onClick={() => deleteFlashcard(collection, id)}
-                  >
-                    Delete
-                  </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <div className="col-md-3" />
-        </div>
-      </div> */}
+  
     </React.Fragment>
   );
 };
